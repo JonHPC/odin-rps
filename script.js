983d5410b875fa26b75a2f6473f6cb79b1ce01c8
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-01-03 16:04:39 -0800

    CREATE initial UI and CONNECT JS

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //Basic Rock Paper Scissors game against the computer
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let buttonChoice = button.id.toLowerCase();
+        const playerSelection = buttonChoice.charAt(0).toUpperCase() + buttonChoice.slice(1);
+        const computerSelection = computerPlay();
+        playRound(playerSelection,computerSelection);
+    })
+})
+
+
+
 
 //when this function is called, a random choice will be printed in the console
 function computerPlay(){
@@ -24,35 +36,41 @@ function getRandomInt(min, max){
 
 //this function plays a round of RPS, with the player input compared against the value from computerPlay
 function playRound(playerSelection, computerSelection){
-    console.log(`You picked ${playerSelection}.`);
-    console.log(`CPU picked ${computerSelection}.`);
+    
+    const playerText = document.querySelector('#playerSelection');
+    playerText.textContent = `You picked ${playerSelection}.`;
+
+    const compText = document.querySelector('#computerSelection');
+    compText.textContent = `CPU picked ${computerSelection}.`;
+
+    const resultsText = document.querySelector('#result');
 
     if(playerSelection == computerSelection){
+        resultsText.textContent = `Both players chose ${playerSelection}! It's a draw!`;
         updateScore(0,0);
-        return `Both players chose ${playerSelection}! It's a draw!`;
     } else if (playerSelection == "Rock"){
         if (computerSelection == "Scissors"){
+            resultsText.textContent = "Rock beats Scissors! You win!";
             updateScore(1,0);
-            return "Rock beats Scissors! You win!";
         } else {
+            resultsText.textContent = "Paper beats Rock! You lose!";
             updateScore(0,1);
-            return "Paper beats Rock! You lose!";
         }
     } else if (playerSelection == "Paper"){
         if(computerSelection == "Rock"){
+            resultsText.textContent = "Paper beats Rock! You win!";
             updateScore(1,0);
-            return "Paper beats Rock! You win!";
         } else {
+            resultsText.textContent = "Scissors beat Paper! You Lose!";
             updateScore(0,1);
-            return "Scissors beat Paper! You Lose!";
         }
     } else if (playerSelection == "Scissors"){
         if(computerSelection == "Paper"){
+            resultsText.textContent = "Scissors beat paper! You win!";
             updateScore(1,0);
-            return "Scissors beat paper! You win!";
         } else {
+            resultsText.textContent = "Rock beats Scissors! You lose!";
             updateScore(0,1);
-            return "Rock beats Scissors! You lose!";
         }
     }
     
@@ -60,13 +78,7 @@ function playRound(playerSelection, computerSelection){
 
 
 
-function game(){
-    let playerPrompt = prompt("Type 'Rock', 'Paper', or 'Scissors'.");
-    playerPrompt = playerPrompt.toLowerCase();
-    const playerSelection = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection));
-}
+
 
 let playerScore = 0;
 let compScore = 0;
@@ -74,8 +86,25 @@ let compScore = 0;
 function updateScore(player, comp){
     playerScore += player;
     compScore += comp;
+    const scoreText = document.querySelector('#currentScore');
+    scoreText.textContent = `Current Score: Player - ${playerScore} || CPU - ${compScore}`;
+    
+    let winOrLose = document.querySelector('#winorlose');
 
-    console.log(`Current Score: Player - ${playerScore} || CPU - ${compScore}`);
+    if(playerScore == 5){
+        //player wins
+        winOrLose.textContent = "You beat the CPU! Great Job!";
+        //alert("You beat the CPU! Great job!");
+    } else if(compScore == 5){
+        //cpu wins
+        winOrLose.textContent = "Oh no! The CPU beat you! Try harder next time!";
+        //alert("Oh no! the CPU beat you! Try harder next time!");
+    } else {
+        winOrLose.textContent = "";
+    }
 }
+
+
+
 
 
